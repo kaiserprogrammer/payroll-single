3391ff2e20c47b3e56d3f0b9bd082e285518fe44
Jürgen Bickert <kaiserprogrammer@gmail.com>
2013-04-21 01:43:20 +0200

    pass first commissioned test

--- payroll.lisp
+++ payroll.lisp
@@ -3,20 +3,52 @@
 
 (in-package :payroll)
 
+(defvar *db*)
+
+(defclass memory-db ()
+  ((classification :accessor classification)
+   (method :accessor pay-method
+           :initform (make-instance 'hold-method))
+   (schedule :accessor schedule)))
+
+(defclass commissioned-classification ()
+  ((salary :initarg :salary
+           :accessor salary)
+   (rate :initarg :rate
+         :accessor rate)))
+
+(defclass hold-method () ())
+(defclass bi-weekly-schedule () ())
+
+(defmethod payment-classification (&optional (db *db*))
+  (classification db))
+(defmethod payment-schedule (&optional (db *db*))
+  (schedule db))
+(defmethod payment-method (&optional (db *db*))
+  (pay-method db))
+
+(defun change-commissioned (salary rate &optional (db *db*))
+  (db-change-commissioned salary rate db))
+
+(defmethod db-change-commissioned (salary rate (db memory-db))
+  (setf (classification db) (make-instance 'commissioned-classification
+                                           :salary salary
+                                           :rate rate))
+  (setf (schedule db) (make-instance 'bi-weekly-schedule)))
 
 
 (remove-tests :all)
 
 (define-test creating-commissioned-employee
   (let ((*db* (make-instance 'memory-db)))
-    (change-commissioned "Johnny" "Work" 250.0 300.)
+    (change-commissioned 250.0 300.0)
     (let ((pc (payment-classification)))
       (assert-equal 250.0 (salary pc))
       (assert-equal 300.0 (rate pc)))
     (assert-eql 'payroll::hold-method
                 (class-name (class-of (payment-method))))
     (assert-eql 'payroll::bi-weekly-schedule
-                (class-name (class-of (schedule))))))
+                (class-name (class-of (payment-schedule))))))
 
 (let ((*print-failures* t)
       (*print-errors* t))
