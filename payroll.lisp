4ad8990ab4080b3a8cb02960fbbf5734da775397
Jürgen Bickert <juergenbickert@gmail.com>
2013-04-24 05:48:56 +0200

    add adding timecards

--- payroll.lisp
+++ payroll.lisp
@@ -11,6 +11,12 @@
    (amount :initarg :amount
            :reader amount)))
 
+(defclass timecard ()
+  ((date :initarg :date
+         :reader date)
+   (hours :initarg :hours
+          :reader hours)))
+
 (defclass memory-db ()
   ((classification :accessor classification)
    (method :accessor pay-method
@@ -27,7 +33,12 @@
 
 (defclass hourly-classification ()
   ((rate :initarg :hourly-rate
-         :accessor hourly-rate)))
+         :accessor hourly-rate)
+   (timecards :initform (make-hash-table :test #'equalp)
+              :reader timecards)))
+
+(defmethod timecard ((pc hourly-classification) (d local-time:timestamp))
+  (gethash d (timecards pc)))
 
 (defclass salaried-classification ()
   ((salary :initarg :salary
@@ -58,6 +69,13 @@
 (defun add-sales-receipt (date amount &optional (db *db*))
   (db-add-sale (make-instance 'sale :date date :amount amount)
                db))
+
+(defun add-timecard (date hours &optional (db *db*))
+  (db-add-timecard (make-instance 'timecard :date date :hours hours)
+                   db))
+
+(defmethod db-add-timecard (tc (db memory-db))
+  (setf (gethash (date tc) (timecards (payment-classification db))) tc))
 
 (defmethod db-add-sale ((sale sale) (db memory-db))
   (push sale (sales-receipts (payment-classification db))))
@@ -115,6 +133,15 @@
       (assert-true sales)
       (assert-equal 500.0 (amount (first sales)))
       (assert-equality #'local-time:timestamp= date (date (first sales))))))
+
+(define-test adding-time-cards
+  (let ((*db* (make-instance 'memory-db))
+        (date (local-time:parse-timestring "2012-12-31")))
+    (change-hourly 12.75)
+    (add-timecard date 8.0)
+    (let ((tc (timecard (payment-classification) date)))
+      (assert-equal 8.0 (hours tc))
+      (assert-equality #'local-time:timestamp= date (date tc)))))
 
 (let ((*print-failures* t)
       (*print-errors* t))
